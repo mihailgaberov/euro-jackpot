cbcd62e16c6ed7610ed9c8533e442d311e2c527f
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-06-04 16:39:35 +0200

    Added watcher for the SASS changes. Introduced _variables.

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -15,8 +15,4 @@ gulp.task('watch', ['serve'], function() {
   gulp.watch(paths.source, ['build-system', browserSync.reload]).on('change', reportChange);
   gulp.watch(paths.html, ['build-html', browserSync.reload]).on('change', reportChange);
   gulp.watch(paths.style, ['build-css']).on('change', reportChange);
-  //gulp.watch(paths.style, function() {
-  //  return gulp.src(paths.style)
-  //    .pipe(browserSync.stream());
-  //}).on('change', reportChange);
 });
